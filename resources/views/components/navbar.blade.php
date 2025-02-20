7577c0bd75b376ffd4ac85942c4f6e69d1ffaf45
mahdi2662 <madialkhalidi83@gmail.com>
2025-02-20 22:18:35 +0100

    heb ook een link naar maintenace gedaan

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,6 +2,7 @@
     <div class="container mx-auto flex justify-between items-center px-4">
         <a href="{{ url('/') }}" class="text-2xl font-bold">TravelEasy</a>
         <div>
+            <a href="{{ url('/maintenance') }}" class="hover:underline">Homepagina Maintenance</a>
             <a href="{{ url('/account-overview')}}"class="ml-4 hover:underline">Accounts</a>
             <a href="{{ url('/communications') }}" class="ml-4 hover:underline">Berichten</a>
             <a href="{{ url('/travels') }}" class="ml-4 hover:underline">Reizen</a>

4141ccb08e5c40907435779e4fdc7fb9fdb9af8c
Fatih Kuzu <337920@student.mboutrecht.nl>
2025-03-18 17:46:47 +0100

    De reisseeder is veranderd

--- database/seeders/TravelsSeeder.php
+++ database/seeders/TravelsSeeder.php
@@ -30,6 +30,21 @@ class TravelsSeeder extends Seeder
                 'updated_at' => Carbon::now(),
             ],
             [
+                'employee_id' => 1, // Zorg ervoor dat dit ID bestaat in de employees-tabel
+                'departure_id' => 2, // Zorg ervoor dat dit ID bestaat in de departures-tabel
+                'destination_id' => 1, // Zorg ervoor dat dit ID bestaat in de destinations-tabel
+                'flight_number' => 'KL5678',
+                'departure_date' => '2025-03-25',
+                'departure_time' => '15:30:00',
+                'arrival_date' => '2025-03-25',
+                'arrival_time' => '18:45:00',
+                'travel_status' => 'Gepland',
+                'is_active' => true,
+                'note' => 'Zakelijke vlucht',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
                 'employee_id' => 2,
                 'departure_id' => 2,
                 'destination_id' => 2,

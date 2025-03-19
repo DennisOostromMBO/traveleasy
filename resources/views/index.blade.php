2943f41c9b11e231e7d7439e44504c65e2783810
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-03-19 10:11:39 +0100

    opzoeken op prijs werkt

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,3 @@
-<!-- filepath: /C:/Users/danie/OneDrive/Documenten/school mappen/Leerjaar 2/Project/Periode 3/traveleasy/resources/views/index.blade.php -->
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -36,6 +35,8 @@
                 <!-- Search Bar -->
                 <form action="{{ url('/bookings') }}" method="GET" class="mt-8 flex justify-center">
                     <input type="text" name="search" placeholder="Zoek bestemming" class="py-3 px-6 rounded-l-full border-none focus:outline-none w-64">
+                    <input type="number" name="min_price" placeholder="Min Prijs" class="py-3 px-1 border-none focus:outline-none w-24">
+                    <input type="number" name="max_price" placeholder="Max Prijs" class="py-3 px-1 border-none focus:outline-none w-24">
                     <button type="submit" class="bg-blue-600 text-white py-3 px-6 rounded-r-full hover:bg-blue-700 transition">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m1.35-5.65a7 7 0 11-14 0 7 7 0 0114 0z" />

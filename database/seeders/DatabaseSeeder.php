548c968c44b710dce455b76cccf36bc14c71574e
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-03-20 11:06:07 +0100

    sliep gister

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Person;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,8 +31,24 @@ class DatabaseSeeder extends Seeder
             InvoiceSeeder::class,
         ]);
 
+        // Create a specific person
+        $person = Person::create([
+            'first_name' => 'Man',
+            'middle_name' => 'Ag',
+            'last_name' => 'Er',
+            'date_of_birth' => '1990-01-01', // Voeg een waarde toe voor date_of_birth
+            'is_active' => 1, // Voeg een waarde toe voor is_active
+        ]);
+
+        // Create a specific user linked to the person
+        User::create([
+            'person_id' => $person->id,
+            'role_id' => 1,
+            'email' => 'admin@example.com',
+            'password' => Hash::make('1'),
+            'is_active' => 1,
+        ]);
 
         \App\Models\User::factory(10)->create();
-
     }
 }
